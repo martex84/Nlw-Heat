4f720c5ddf67ab672d8d58c1819b5dbc898af4e7
martex84 <martex849@gmail.com>
2021-10-19 19:22:18 -0300

    Inclusao Controller

---
+++ backend/src/controller/MessagensController.ts
@@ -0,0 +1,10 @@
+import { Request, Response } from 'express';
+import { MessagensServices } from '../services/MessagensServices'
+
+class AuthenticateUserController {
+    async handle(request: Request, response: Response) {
+
+    }
+}
+
+export { AuthenticateUserController };
