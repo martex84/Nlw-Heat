4bab3023d821ac21c1a9f42d9b044793d56b33ac
martex84 <martex849@gmail.com>
2021-10-19 23:17:26 -0300

    Correcao Controller

--- backend/src/controller/MessagensController.ts
+++ backend/src/controller/MessagensController.ts
@@ -1,10 +1,19 @@
 import { Request, Response } from 'express';
 import { MessagensServices } from '../services/MessagensServices'
 
-class AuthenticateUserController {
+class MessagensController {
     async handle(request: Request, response: Response) {
+        const { message } = request.body;
+        const { user_id } = request;
+
+        const service = new MessagensServices();
+
+        const result = service.execute(message, user_id);
+
+        return response.json(result);
+
 
     }
 }
 
-export { AuthenticateUserController };
+export { MessagensController };
