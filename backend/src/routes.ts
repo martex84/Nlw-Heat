ae2c9d3ed80a076300f7fa70d2518697600cec03
martex84 <martex849@gmail.com>
2021-10-19 23:05:15 -0300

    Inclusao Rota Messagem

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -1,8 +1,11 @@
 import { Router } from "express";
 import { AuthenticateUserController } from "./controller/AuthenticateUserController";
+import EnsureAutheticate from "./middleware/EnsureAuthenticate";
 
 const router = Router();
 
 router.post("/authenticate", new AuthenticateUserController().handle);
+
+router.post("/messages", EnsureAutheticate, new AuthenticateUserController().handle);
 
 export { router };
