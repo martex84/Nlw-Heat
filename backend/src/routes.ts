4c3f301b65cbdcdbcfcc4f6fc32dedb0eefde592
martex84 <martex849@gmail.com>
2021-10-20 02:43:18 -0300

    Inclusao Rotas Last Menssage

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -1,5 +1,6 @@
 import { Router } from "express";
 import { AuthenticateUserController } from "./controller/AuthenticateUserController";
+import { GetLast3MessagensController } from "./controller/GetLast3MessagensController";
 import { MessagensController } from "./controller/MessagensController";
 import EnsureAutheticate from "./middleware/EnsureAuthenticate";
 
@@ -8,5 +9,7 @@ const router = Router();
 router.post("/authenticate", new AuthenticateUserController().handle);
 
 router.post("/messages", EnsureAutheticate, new MessagensController().handle);
+
+router.get("/messagens/last3", new GetLast3MessagensController().handle);
 
 export { router };
