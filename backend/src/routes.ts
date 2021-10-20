b58a909f9f53bfa796884d6aa7daf0bc2606676e
martex84 <martex849@gmail.com>
2021-10-19 23:27:22 -0300

    Correcao Rota Message

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -1,11 +1,12 @@
 import { Router } from "express";
 import { AuthenticateUserController } from "./controller/AuthenticateUserController";
+import { MessagensController } from "./controller/MessagensController";
 import EnsureAutheticate from "./middleware/EnsureAuthenticate";
 
 const router = Router();
 
 router.post("/authenticate", new AuthenticateUserController().handle);
 
-router.post("/messages", EnsureAutheticate, new AuthenticateUserController().handle);
+router.post("/messages", EnsureAutheticate, new MessagensController().handle);
 
 export { router };
