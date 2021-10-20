3174a2e8760fe6285c19b68474e1186c76bab245
martex84 <martex849@gmail.com>
2021-10-20 17:17:26 -0300

    Conclusao User Profile

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -2,6 +2,7 @@ import { Router } from "express";
 import { AuthenticateUserController } from "./controller/AuthenticateUserController";
 import { GetLast3MessagensController } from "./controller/GetLast3MessagensController";
 import { MessagensController } from "./controller/MessagensController";
+import { UserProfileController } from "./controller/UserProfileController";
 import EnsureAutheticate from "./middleware/EnsureAuthenticate";
 
 const router = Router();
@@ -11,5 +12,7 @@ router.post("/authenticate", new AuthenticateUserController().handle);
 router.post("/messages", EnsureAutheticate, new MessagensController().handle);
 
 router.get("/messagens/last3", new GetLast3MessagensController().handle);
+
+router.get("/profile", EnsureAutheticate, new UserProfileController().handle);
 
 export { router };
