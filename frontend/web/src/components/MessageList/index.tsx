305325700405d7a3fa5371015f0dd3a00ca0b8f8
martex84 <martex849@gmail.com>
2021-10-21 01:55:20 -0300

    Inclusao Sistema Mensagens

--- frontend/web/src/components/MessageList/index.tsx
+++ frontend/web/src/components/MessageList/index.tsx
@@ -1,58 +1,53 @@
-import style from "./styles.module.scss";
+import { useEffect, useState } from 'react'
 
+import { api } from "../../services/api";
+
+import style from "./styles.module.scss";
 import logoImg from '../../assets/logo.svg';
 
+type Message = {
+    id: string;
+    text: string;
+    user: {
+        name: string;
+        avatar_url: string;
+    }
+}
+
 export function MessageList() {
+
+    const [messagens, setMessagens] = useState<Message[]>([])
+
+    useEffect(() => {
+        api.get<Message[]>("/messagens/last3").then(response => {
+            setMessagens(response.data)
+        })
+    }, [])
+
     return (
         <>
             <div className={style.messageListWrapper}>
                 <img src={logoImg} alt="DoWhite 2021" />
 
                 <ul className={style.messageList}>
-                    <li className={style.message}>
-                        <p className={style.messageContent}>
-                            Mensagem
-                        </p>
+                    {messagens.map(data => {
+                        return (
+                            <li className={style.message} key={data.id}>
+                                <p className={style.messageContent}>
+                                    {data.text}
+                                </p>
 
-                        <div className={style.messageUser}>
-                            <div className={style.userImage}>
-                                <img src="https://github.com/martex84.png" alt="Martex" />
-                            </div>
-                            <span>
-                                Martex
-                            </span>
-                        </div>
-                    </li>
-
-                    <li className={style.message}>
-                        <p className={style.messageContent}>
-                            Mensagem
-                        </p>
-
-                        <div className={style.messageUser}>
-                            <div className={style.userImage}>
-                                <img src="https://github.com/martex84.png" alt="Martex" />
-                            </div>
-                            <span>
-                                Martex
-                            </span>
-                        </div>
-                    </li>
-
-                    <li className={style.message}>
-                        <p className={style.messageContent}>
-                            Mensagem
-                        </p>
-
-                        <div className={style.messageUser}>
-                            <div className={style.userImage}>
-                                <img src="https://github.com/martex84.png" alt="Martex" />
-                            </div>
-                            <span>
-                                Martex
-                            </span>
-                        </div>
-                    </li>
+                                <div className={style.messageUser}>
+                                    <div className={style.userImage}>
+                                        <img src={data.user.avatar_url} alt={data.user.name} />
+                                    </div>
+                                    <span>
+                                        {data.user.name}
+                                    </span>
+                                </div>
+                            </li>
+                        )
+                    })}
                 </ul>
             </div>
         </>
