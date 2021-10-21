d1c987df9502d1a09cd2edc9785ad853f41c8863
martex84 <martex849@gmail.com>
2021-10-21 14:43:27 -0300

    Inclusao Rotas App

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -25,14 +25,4 @@ app.use(express.json());
 
 app.use(router);
 
-app.get('/github', (request, response) => {
-    response.redirect(`http://github.com/login/oauth/authorize?client_id=${process.env.GITHUB_CLIENT_ID}`);
-})
-
-app.get('/signin/callback', (request, response) => {
-    const { code } = request.query;
-
-    response.json(code);
-})
-
 export { serverHttp, io };
