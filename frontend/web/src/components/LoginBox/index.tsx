71b41df5c99632f7fbfddc67de148a6ec3d35ab5
martex84 <martex849@gmail.com>
2021-10-20 22:12:58 -0300

    Conclusao Login Box

--- frontend/web/src/components/LoginBox/index.tsx
+++ frontend/web/src/components/LoginBox/index.tsx
@@ -1,3 +1,5 @@
+import { VscGithubInverted } from "react-icons/vsc"
+
 import style from './styles.module.scss'
 
 export function LoginBox() {
@@ -8,7 +10,8 @@ export function LoginBox() {
                     Entre  e compartilhe sua mensagem
                 </strong>
                 <a href="#" className={style.signInWithGithub}>
-
+                    <VscGithubInverted size="24" />
+                    Entrar com GitHub
                 </a>
             </div>
         </>
