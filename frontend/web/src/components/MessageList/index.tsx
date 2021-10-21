f4dd54c303e79eeea24e101f1119e6d75bee649a
martex84 <martex849@gmail.com>
2021-10-21 00:54:47 -0300

    Conclusao Style Home

--- frontend/web/src/components/MessageList/index.tsx
+++ frontend/web/src/components/MessageList/index.tsx
@@ -1,9 +1,60 @@
+import style from "./styles.module.scss";
+
+import logoImg from '../../assets/logo.svg';
+
 export function MessageList() {
     return (
         <>
-            <h1>
-                Message List
-            </h1>
+            <div className={style.messageListWrapper}>
+                <img src={logoImg} alt="DoWhite 2021" />
+
+                <ul className={style.messageList}>
+                    <li className={style.message}>
+                        <p className={style.messageContent}>
+                            Mensagem
+                        </p>
+
+                        <div className={style.messageUser}>
+                            <div className={style.userImage}>
+                                <img src="https://github.com/martex84.png" alt="Martex" />
+                            </div>
+                            <span>
+                                Martex
+                            </span>
+                        </div>
+                    </li>
+
+                    <li className={style.message}>
+                        <p className={style.messageContent}>
+                            Mensagem
+                        </p>
+
+                        <div className={style.messageUser}>
+                            <div className={style.userImage}>
+                                <img src="https://github.com/martex84.png" alt="Martex" />
+                            </div>
+                            <span>
+                                Martex
+                            </span>
+                        </div>
+                    </li>
+
+                    <li className={style.message}>
+                        <p className={style.messageContent}>
+                            Mensagem
+                        </p>
+
+                        <div className={style.messageUser}>
+                            <div className={style.userImage}>
+                                <img src="https://github.com/martex84.png" alt="Martex" />
+                            </div>
+                            <span>
+                                Martex
+                            </span>
+                        </div>
+                    </li>
+                </ul>
+            </div>
         </>
     );
 }
